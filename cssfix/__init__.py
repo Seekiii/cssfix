f135afe84eb36860854e0cdd86b3c2f850c1b46f
šeki <seekii.official@gmail.com>
2025-07-17 17:44:59 +0200

    1.1.0

--- cssfix/__init__.py
+++ cssfix/__init__.py
@@ -1,23 +1,27 @@
 import re
 
-class css:
-    def __init__(self, css_text):
-        self.css_text = css_text
-        self.cleaned_css = self.remove_comments(css_text)
-        self.rules = self.parse_rules(self.cleaned_css)
-        self.merged = self.merge_rules(self.rules)
+class css(str):
+    def __new__(cls, css_text):
+        cleaned_css = cls.remove_comments(css_text)
+        rules = cls.parse_rules(cleaned_css)
+        merged = cls.merge_rules(rules)
+        optimized = ''.join(f'{selector}{{{props.replace(" ", "")}}}' for selector, props in merged.items())
+        return str.__new__(cls, optimized)
 
-    def remove_comments(self, css):
+    @staticmethod
+    def remove_comments(css):
         css = re.sub(r'/\*.*?\*/', '', css, flags=re.DOTALL)
         css = css.replace("\n", "")
         return css
 
-    def parse_rules(self, css):
+    @staticmethod
+    def parse_rules(css):
         pattern = re.compile(r'([^{]+)\{([^}]+)\}')
         rules = pattern.findall(css)
         return [(selector.strip(), properties.strip()) for selector, properties in rules]
 
-    def merge_properties(self, props1, props2):
+    @staticmethod
+    def merge_properties(props1, props2):
         props_dict = {}
         for prop in props1.split(';'):
             if ':' in prop:
@@ -29,7 +33,8 @@ class css:
                 props_dict[k.strip()] = v.strip()
         return '; '.join(f'{k}: {v}' for k, v in props_dict.items() if k) + ';'
 
-    def merge_rules(self, rules):
+    @classmethod
+    def merge_rules(cls, rules):
         merged = {}
         for selector, props in rules:
             if not props.strip():
@@ -37,10 +42,7 @@ class css:
             selectors = [s.strip() for s in selector.split(',')]
             for sel in selectors:
                 if sel in merged:
-                    merged[sel] = self.merge_properties(merged[sel], props)
+                    merged[sel] = cls.merge_properties(merged[sel], props)
                 else:
                     merged[sel] = props
         return merged
-
-    def get_optimized_css(self):
-        return ''.join(f'{selector}{{{props.replace(" ", "")}}}' for selector, props in self.merged.items())
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cssfix',
-    version='1.0.0',
+    version='1.1.0',
     packages=find_packages(),
     description='Simple CSS optimizer module for merging class properties',
     author='seekii',
